eff8821514ea2441b30eff99b5ab56c4a23304e3
HK <Email>
2019-08-27 16:53:10 +0900

    DiceImage on Monopoly

---
+++ gamedata/monopoly/MonopolyPanel.java
@@ -0,0 +1,34 @@
+package gamedata.monopoly;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+import source.CGPlayer;
+import source.display.PlayPanel;
+
+public class MonopolyPanel extends PlayPanel {
+    private static final long serialVersionUID = 1L;
+    private MonopolyPlayer player;
+
+    @Override
+    public void setup(CGPlayer player){
+        this.player = (MonopolyPlayer)player;
+    }
+
+    @Override
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+
+        g.setColor(Color.WHITE);
+        g.fillRect(0, 0, getWidth(), getHeight());
+
+
+        drawDice(g, 100, 100, 100, 100, 1);
+        drawDice(g, 300, 100, 100, 100, 2);
+        drawDice(g, 500, 100, 100, 100, 3);
+        drawDice(g, 100, 300, 100, 100, 4);
+        drawDice(g, 300, 300, 100, 100, 5);
+        drawDice(g, 500, 300, 100, 100, 6);
+    }
+
+}
