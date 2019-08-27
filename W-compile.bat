0548ef46ffbefebc1b10c9a2c4ed2a122316c0e8
HK <Email>
2019-08-27 17:08:52 +0900

    Update W-compile.bat

--- W-compile.bat
+++ W-compile.bat
@@ -1,3 +1,9 @@
 @echo off
-javac -d ./class -encoding UTF-8 ./Main.java ./gamedata/daifugo/*.java ./gamedata/monopoly/*.java
+echo compile Main
+javac -d ./class -encoding UTF-8 ./Main.java
+for /d %%a in (./gamedata/*) do (
+    echo compile %%a
+    javac -d ./class -encoding UTF-8 ./gamedata/%%a/*.java
+)
+
 pause
